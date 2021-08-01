7cd4abc04b6b9be06bbdb5ca61f60c8d7c368b1f
kumarrahul1998 <85093268+kumarrahul1998@users.noreply.github.com>
2021-08-01 10:39:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const students = [
+  { name: "Adhi", gender: "male" },
+  { name: "Ahamed", gender: "male" },
+  { name: "Rutika Kadam", gender: "female" },
+  { name: "Priyadharsini", gender: "female" },
+];
+
+const femaleNames= students.filter(i=>i.gender==="female")
+
+const startingANames= students.filter(i=>i.name.charAt(0)==="A")
+
+const malesArr= students.filter(i=>i.gender==="male");
+
+const maleCount= malesArr.length
+
+console.log('Female Names',femaleNames);
+console.log('Names Starting with A',startingANames)
+console.log("Male Count",maleCount)
